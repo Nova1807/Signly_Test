b7b23ee4a8ffc3ee68782b9a6ada8cc4a51c8f56
Erik Hauer <erik@MacBook-Air-von-Erik.local>
2025-12-02 10:23:25 +0100

    v1.6

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from "@nestjs/common";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
   }))
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
