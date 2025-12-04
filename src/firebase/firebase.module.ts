1b1978ddf81d0a3d8d7625ffaee8d709862bc9d6
Erik Hauer <erik@MacBook-Air-von-Erik.local>
2025-12-04 10:40:49 +0100

    gcfhvbjn

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -9,10 +9,24 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       provide: 'FIREBASE_APP',
       useFactory: (configService: ConfigService) => {
         if (!admin.apps.length) {
+          const projectId =
+            configService.get<string>('FIREBASE_PROJECT_ID') ||
+            configService.get<string>('PROJECT_ID') ||
+            'signly-be33f'; // hier dein GCP/Firebase-Projekt eintragen
+
           admin.initializeApp({
             credential: admin.credential.applicationDefault(),
+            projectId,
           });
+
+          // Falls du sicherstellen willst, dass die Standard-DB verwendet wird:
+          admin.firestore().settings({
+            databaseId: '(default)',
+          });
+
+          console.log('FIREBASE INIT', { projectId });
         }
+
         return admin.app();
       },
       inject: [ConfigService],
