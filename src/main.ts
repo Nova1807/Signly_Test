1cb1977f78255b7dc7abddefcbfe0ec28313d5f2
Erik Hauer <erik@MacBook-Air-von-Erik.local>
2026-02-12 16:11:18 +0100

    <ywasex

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Wichtig für Sign in with Apple POST Callback (application/x-www-form-urlencoded)
+  app.use(express.urlencoded({ extended: true }));
+  // Standard JSON parsing (für deine normalen APIs)
+  app.use(express.json());
 
   app.useGlobalPipes(
     new ValidationPipe({
